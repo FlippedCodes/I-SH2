469e00dd6c7d1c327239f671ad352668742b1cd4
Phil <philip.schaefer0@gmail.com>
2026-03-26 00:18:15 +0100

    chore: code style

--- src/api/src/routes/v1/index.ts
+++ src/api/src/routes/v1/index.ts
@@ -24,17 +24,24 @@ v1.route('/hubs', hubsRouter);
 
 v1.route('/users', usersRouter);
 
-v1.get('/healthcheck', (c) => c.json({
-  status: 'success',
-  message: 'Healthcheck successful',
-}));
+v1.get('/healthcheck', (c) =>
+  c.json({
+    status: 'success',
+    message: 'Healthcheck successful',
+  }),
+);
 
 // https://www.npmjs.com/package/hono-error-handler
-v1.onError(errorHandler([], (err, c) => {
-  console.error(`${err.stack}`);
-  let stack;
-  if (err.statusCode === 404) stack = err.stack;
-  return c.json({ success: false, error: { message: err.message || 'Internal server error', stack } }, err.statusCode as StatusCode || 500);
-}));
+v1.onError(
+  errorHandler([], (err, c) => {
+    console.error(`${err.stack}`);
+    let stack;
+    if (err.statusCode === 404) stack = err.stack;
+    return c.json(
+      { success: false, error: { message: err.message || 'Internal server error', stack } },
+      (err.statusCode as StatusCode) || 500,
+    );
+  }),
+);
 
 export default v1;
